40ed5b8ec3ff248fa50218eee16af90745897a16
Trigo YODILA <trigoyodila1996@gmail.com>
2023-03-14 18:53:28 +0100

    Feat:create actions with bindActionCreators

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const redux = require('redux')
 
 //create store
 const createStore = redux.createStore
+//creater of actions
+const bindActionsCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
     quantity: 1,
   };
+}
+
+function restockedCake(qty=1){
+    return {
+        type : CAKE_RESTOCKED,
+        payload:qty
+    }
 }
 
 //initial state
@@ -27,7 +37,12 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,   //copie previousState
         numOfCakes: state.numOfCakes - 1, //update a property
-      };
+      }
+    case CAKE_RESTOCKED:
+        return {
+            ...state,
+            numOfCakes: state.numOfCakes + action.payload
+        }
     default :
       return state
   }
@@ -47,13 +62,23 @@ const unsubscribe = store.subscribe(() =>
 
 //repartition of state
 // send action in reducer
-store.dispatch(orderCake()) // or dispatch with creater of action
+//store.dispatch(orderCake()) // or dispatch with creater of action
 // store.dispatch({
 //     type:CAKE_ORDERED,
 //     quantity:1
 // })
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockedCake(3))
+
+//used bindActionscreators
+const actions = bindActionsCreators({orderCake, restockedCake}, store.dispatch)
+//call actions
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockedCake(3)
 
 //unsubscribe in all update in store
 unsubscribe()
