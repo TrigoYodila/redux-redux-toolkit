035ff1c955ecd4e2a98250fefe3296238254d62d
Trigo YODILA <trigoyodila1996@gmail.com>
2023-03-14 23:55:05 +0100

    Feat: used many reducers

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const redux = require('redux')
 const createStore = redux.createStore
 //creater of actions
 const bindActionsCreators = redux.bindActionCreators
+//create a combine reducers
+const combineReducers = redux.combineReducers
 
 const CAKE_ORDERED = "CAKE_ORDERED"
 const CAKE_RESTOCKED = "CAKE_RESTOCKED"
+const ICECREAM_ORDERED = "ICECREAM_ORDERED"
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED"
 
 function orderCake() {
   return {
@@ -23,33 +27,81 @@ function restockedCake(qty=1){
     }
 }
 
+function orderIceCream(qty=1){
+    return {
+        type:ICECREAM_ORDERED,
+        payload:qty
+    }
+}
+
+function restockedIceCream(qty=1){
+    return {
+        type:ICECREAM_RESTOCKED,
+        payload:qty
+    }
+}
+
 //initial state
-const initialState = {
-  numOfCakes: 10,
-  anotherProperty:0,
-};
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams:20,
+// };
+
+//separate initial state and reducers
+const initialCakeState = {
+    numOfCakes:10
+}
+
+const initialIceCreamState = {
+    numOfIceCreams:20
+}
 
 //previousState + action => new state
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case CAKE_ORDERED:
-      return {
-        ...state,   //copie previousState
-        numOfCakes: state.numOfCakes - 1, //update a property
-      }
-    case CAKE_RESTOCKED:
+const cakeReducer = (state = initialCakeState, action) => {
+    switch (action.type) {
+      case CAKE_ORDERED:
         return {
-            ...state,
-            numOfCakes: state.numOfCakes + action.payload
-        }
-    default :
-      return state
+          ...state, //copie previousState
+          numOfCakes: state.numOfCakes - 1, //update a property
+        };
+      case CAKE_RESTOCKED:
+        return {
+          ...state,
+          numOfCakes: state.numOfCakes + action.payload,
+        };
+      default:
+        return state;
+    }
+}
+
+const iceCreamreducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+    default:
+      return state;
   }
 };
 
+//combine reducers
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamreducer
+})
+
+
 //create store
-const store = createStore(reducer)
+// const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 //expose state with getState method and save that initialstate in console
 console.log('Initial state ', store.getState())
@@ -73,12 +125,17 @@ const unsubscribe = store.subscribe(() =>
 // store.dispatch(restockedCake(3))
 
 //used bindActionscreators
-const actions = bindActionsCreators({orderCake, restockedCake}, store.dispatch)
+const actions = bindActionsCreators({orderCake, restockedCake, orderIceCream, restockedIceCream}, store.dispatch)
 //call actions
 actions.orderCake()
 actions.orderCake()
 actions.orderCake()
 actions.restockedCake(3)
+
+actions.orderIceCream()
+actions.orderIceCream()
+actions.orderIceCream()
+actions.orderIceCream(3)
 
 //unsubscribe in all update in store
 unsubscribe()
