e92b2a466039545ba1ec29ed62cdd93a8cbac0c5
Trigo YODILA <trigoyodila1996@gmail.com>
2023-03-14 17:28:44 +0100

    Feat:create store and responsabilities

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const CAKE_ORDERED = "CAKE_ORDERED";
+//import redux
+const redux = require('redux')
+
+//create store
+const createStore = redux.createStore
+
+const CAKE_ORDERED = "CAKE_ORDERED"
 
 function orderCake() {
   return {
@@ -26,3 +32,29 @@ const reducer = (state = initialState, action) => {
       return state
   }
 };
+
+//create store
+const store = createStore(reducer)
+
+//expose state with getState method and save that initialstate in console
+console.log('Initial state ', store.getState())
+
+//configure a listener for store
+//save the state in console after each update 
+const unsubscribe = store.subscribe(() =>
+  console.log("update state ", store.getState())
+);
+
+//repartition of state
+// send action in reducer
+store.dispatch(orderCake()) // or dispatch with creater of action
+// store.dispatch({
+//     type:CAKE_ORDERED,
+//     quantity:1
+// })
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+//unsubscribe in all update in store
+unsubscribe()
+
