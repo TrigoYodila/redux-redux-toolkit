5d57dfa0b429a09d24564dac55f793f99ed2d82b
Trigo YODILA <trigoyodila1996@gmail.com>
2023-03-17 15:47:43 +0100

    feat:create bases components

---
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const IceCreamView = () => {
+  return (
+    <div>
+      <h2>Number of ice creams -</h2>
+      <button>Ordered ice cream</button>
+      <button>Restock ice cream</button>
+    </div>
+  );
+};
+
+export default IceCreamView
